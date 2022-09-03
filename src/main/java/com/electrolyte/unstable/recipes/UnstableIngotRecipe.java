03733c71aa62f231b25f729ca6f17b1c0b56ff24
Electrolyte220 <cjm03c@gmail.com>
2022-09-03 16:53:01 -0400

    Fix Unstable Ingot Recipe, add Etheric Sword Recipes.

--- src/main/java/com/electrolyte/unstable/recipes/UnstableIngotRecipe.java
+++ src/main/java/com/electrolyte/unstable/recipes/UnstableIngotRecipe.java
@@ -1,6 +1,5 @@
 package com.electrolyte.unstable.recipes;
 
-import com.electrolyte.unstable.init.ModRecipes;
 import com.google.common.annotations.VisibleForTesting;
 import com.google.common.collect.Maps;
 import com.google.common.collect.Sets;
@@ -14,103 +13,31 @@ import net.minecraft.resources.ResourceLocation;
 import net.minecraft.util.GsonHelper;
 import net.minecraft.world.inventory.CraftingContainer;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.crafting.CraftingRecipe;
 import net.minecraft.world.item.crafting.Ingredient;
 import net.minecraft.world.item.crafting.RecipeSerializer;
+import net.minecraft.world.item.crafting.ShapedRecipe;
 import net.minecraft.world.level.Level;
 import net.minecraftforge.common.ForgeHooks;
-import net.minecraftforge.common.crafting.IShapedRecipe;
 import net.minecraftforge.registries.ForgeRegistryEntry;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.Map;
 import java.util.Set;
 
-public class UnstableIngotRecipe implements CraftingRecipe, IShapedRecipe<CraftingContainer> {
+public class UnstableIngotRecipe extends ShapedRecipe {
 
-    private final ResourceLocation location;
-    private final NonNullList<Ingredient> input;
-    private final ItemStack result;
-    
-    static final int MAX_WIDTH = 3, MAX_HEIGHT = 3;
-    final int width, height;
 
-    public UnstableIngotRecipe(ResourceLocation location, int width, int height, NonNullList<Ingredient> input, ItemStack result) {
-        this.location = location;
-        this.width = width;
-        this.height = height;
-        this.input = input;
-        this.result = result;
+    public UnstableIngotRecipe(ResourceLocation pId, String pGroup, int pWidth, int pHeight, NonNullList<Ingredient> pRecipeItems, ItemStack pResult) {
+        super(pId, pGroup, pWidth, pHeight, pRecipeItems, pResult);
     }
+
 
     @Override
-    public boolean matches(CraftingContainer pInv, @NotNull Level pLevel) {
-        for(int i = 0; i <= pInv.getWidth() - this.width; ++i) {
-            for(int j = 0; j <= pInv.getHeight() - this.height; ++j) {
-                if (this.matches(pInv, i, j, true)) {
-                    return true;
-                }
-
-                if (this.matches(pInv, i, j, false)) {
-                    return true;
-                }
-            }
-        }
-
-        return false;
-    }
-    private boolean matches(CraftingContainer pCraftingInventory, int pWidth, int pHeight, boolean pMirrored) {
+    public boolean matches(CraftingContainer pCraftingInventory, Level level) {
         if(pCraftingInventory.getClass() != CraftingContainer.class) return false;
-        for(int i = 0; i < pCraftingInventory.getWidth(); ++i) {
-            for(int j = 0; j < pCraftingInventory.getHeight(); ++j) {
-                int k = i - pWidth;
-                int l = j - pHeight;
-                Ingredient ingredient = Ingredient.EMPTY;
-                if (k >= 0 && l >= 0 && k < this.width && l < this.height) {
-                    if (pMirrored) {
-                        ingredient = this.input.get(this.width - k - 1 + l * this.width);
-                    } else {
-                        ingredient = this.input.get(k + l * this.width);
-                    }
-                }
-
-                if (!ingredient.test(pCraftingInventory.getItem(i + j * pCraftingInventory.getWidth()))) {
-                    return false;
-                }
-            }
-        }
-
-        return true;
+        return super.matches(pCraftingInventory, level);
     }
 
-    @Override
-    public @NotNull ItemStack assemble(@NotNull CraftingContainer container) {
-        return this.getResultItem().copy();
-    }
-
-    @Override
-    public boolean canCraftInDimensions(int pWidth, int pHeight) {
-        return pWidth >= this.width && pHeight >= this.height;
-    }
-
-    @Override
-    public @NotNull ItemStack getResultItem() {
-        return this.result;
-    }
-
-    @Override
-    public @NotNull ResourceLocation getId() {
-        return this.location;
-    }
-
-    @Override
-    public @NotNull RecipeSerializer<?> getSerializer() {
-        return ModRecipes.UNSTABLE_INGOT_RECIPE_SERIALIZER.get();
-    }
-
-    public @NotNull NonNullList<Ingredient> getIngredients() {
-        return this.input;
-    }
 
     static NonNullList<Ingredient> dissolvePattern(String[] pPattern, Map<String, Ingredient> pKeys, int pPatternWidth, int pPatternHeight) {
         NonNullList<Ingredient> nonnulllist = NonNullList.withSize(pPatternWidth * pPatternHeight, Ingredient.EMPTY);
@@ -173,52 +100,6 @@ public class UnstableIngotRecipe implements CraftingRecipe, IShapedRecipe<Crafti
         }
     }
 
-    public boolean isIncomplete() {
-        NonNullList<Ingredient> nonnulllist = this.getIngredients();
-        return nonnulllist.isEmpty() || nonnulllist.stream().filter((p_151277_) -> !p_151277_.isEmpty()).anyMatch(ForgeHooks::hasNoElements);
-    }
-
-    private static int firstNonSpace(String pEntry) {
-        int i;
-        for(i = 0; i < pEntry.length() && pEntry.charAt(i) == ' '; ++i) {}
-
-        return i;
-    }
-
-    private static int lastNonSpace(String pEntry) {
-        int i;
-        for(i = pEntry.length() - 1; i >= 0 && pEntry.charAt(i) == ' '; --i) {}
-
-        return i;
-    }
-
-    static String[] patternFromJson(JsonArray pPatternArray) {
-        String[] astring = new String[pPatternArray.size()];
-        if (astring.length > MAX_HEIGHT) {
-            throw new JsonSyntaxException("Invalid pattern: too many rows, " + MAX_HEIGHT + " is maximum");
-        } else if (astring.length == 0) {
-            throw new JsonSyntaxException("Invalid pattern: empty pattern not allowed");
-        } else {
-            for(int i = 0; i < astring.length; ++i) {
-                String s = GsonHelper.convertToString(pPatternArray.get(i), "pattern[" + i + "]");
-                if (s.length() > MAX_WIDTH) {
-                    throw new JsonSyntaxException("Invalid pattern: too many columns, " + MAX_WIDTH + " is maximum");
-                }
-
-                if (i > 0 && astring[0].length() != s.length()) {
-                    throw new JsonSyntaxException("Invalid pattern: each row must be the same width");
-                }
-
-                astring[i] = s;
-            }
-
-            return astring;
-        }
-    }
-
-    /**
-     * Returns a key json object as a Java HashMap.
-     */
     static Map<String, Ingredient> keyFromJson(JsonObject pKeyEntry) {
         Map<String, Ingredient> map = Maps.newHashMap();
 
@@ -238,18 +119,47 @@ public class UnstableIngotRecipe implements CraftingRecipe, IShapedRecipe<Crafti
         return map;
     }
 
-    public static ItemStack itemStackFromJson(JsonObject pStackObject) {
-        return net.minecraftforge.common.crafting.CraftingHelper.getItemStack(pStackObject, true, true);
+    static String[] patternFromJson(JsonArray pPatternArray) {
+        String[] astring = new String[pPatternArray.size()];
+        if (astring.length > 3) {
+            throw new JsonSyntaxException("Invalid pattern: too many rows, 3 is maximum");
+        } else if (astring.length == 0) {
+            throw new JsonSyntaxException("Invalid pattern: empty pattern not allowed");
+        } else {
+            for(int i = 0; i < astring.length; ++i) {
+                String s = GsonHelper.convertToString(pPatternArray.get(i), "pattern[" + i + "]");
+                if (s.length() > 3) {
+                    throw new JsonSyntaxException("Invalid pattern: too many columns, 3 is maximum");
+                }
+
+                if (i > 0 && astring[0].length() != s.length()) {
+                    throw new JsonSyntaxException("Invalid pattern: each row must be the same width");
+                }
+
+                astring[i] = s;
+            }
+
+            return astring;
+        }
     }
 
-    @Override
-    public int getRecipeWidth() {
-        return this.width;
+    public boolean isIncomplete() {
+        NonNullList<Ingredient> nonnulllist = this.getIngredients();
+        return nonnulllist.isEmpty() || nonnulllist.stream().filter((p_151277_) -> !p_151277_.isEmpty()).anyMatch(ForgeHooks::hasNoElements);
     }
 
-    @Override
-    public int getRecipeHeight() {
-        return this.height;
+    private static int firstNonSpace(String pEntry) {
+        int i;
+        for(i = 0; i < pEntry.length() && pEntry.charAt(i) == ' '; ++i) {}
+
+        return i;
+    }
+
+    private static int lastNonSpace(String pEntry) {
+        int i;
+        for(i = pEntry.length() - 1; i >= 0 && pEntry.charAt(i) == ' '; --i) {}
+
+        return i;
     }
 
     public static class Serializer extends ForgeRegistryEntry<RecipeSerializer<?>> implements RecipeSerializer<UnstableIngotRecipe> {
@@ -259,8 +169,8 @@ public class UnstableIngotRecipe implements CraftingRecipe, IShapedRecipe<Crafti
             int i = astring[0].length();
             int j = astring.length;
             NonNullList<Ingredient> nonnulllist = UnstableIngotRecipe.dissolvePattern(astring, map, i, j);
-            ItemStack itemstack = UnstableIngotRecipe.itemStackFromJson(GsonHelper.getAsJsonObject(pJson, "result"));
-            return new UnstableIngotRecipe(pRecipeId, i, j, nonnulllist, itemstack);
+            ItemStack itemstack = ShapedRecipe.itemStackFromJson(GsonHelper.getAsJsonObject(pJson, "result"));
+            return new UnstableIngotRecipe(pRecipeId, "", i, j, nonnulllist, itemstack);
         }
 
         public UnstableIngotRecipe fromNetwork(@NotNull ResourceLocation pRecipeId, FriendlyByteBuf pBuffer) {
@@ -271,18 +181,18 @@ public class UnstableIngotRecipe implements CraftingRecipe, IShapedRecipe<Crafti
             nonnulllist.replaceAll(ignored -> Ingredient.fromNetwork(pBuffer));
 
             ItemStack itemstack = pBuffer.readItem();
-            return new UnstableIngotRecipe(pRecipeId, i, j, nonnulllist, itemstack);
+            return new UnstableIngotRecipe(pRecipeId, "", i, j, nonnulllist, itemstack);
         }
 
         public void toNetwork(FriendlyByteBuf pBuffer, UnstableIngotRecipe pRecipe) {
-            pBuffer.writeVarInt(pRecipe.width);
-            pBuffer.writeVarInt(pRecipe.height);
+            pBuffer.writeVarInt(pRecipe.getRecipeWidth());
+            pBuffer.writeVarInt(pRecipe.getHeight());
 
-            for(Ingredient ingredient : pRecipe.input) {
+            for(Ingredient ingredient : pRecipe.getIngredients()) {
                 ingredient.toNetwork(pBuffer);
             }
 
-            pBuffer.writeItem(pRecipe.result);
+            pBuffer.writeItem(pRecipe.getResultItem());
         }
     }
 }
